d36ca23a69a5bd98295954177151cc2b757a46dd
Su BoRu <kilfu0701@gmail.com>
2013-11-28 11:02:25 +0800

    update tabs options in javascripts.

--- js/options.js
+++ js/options.js
@@ -8,62 +8,73 @@ $(document).ready(function(e) {
     $( "#tabs" ).tabs(function(){ console.log(1); });
     
     // load tmpl files
-    TH.import_tmpl(['simple_option'], function(r) {
-        var opts = {
-            legend: 'Gesture for draging Link',
-            up: {
-                title: 'UP: ',
-                option: [
-                    {value: '0', title: '前景'},
-                    {value: '1', title: '背景'},
-                    {value: '2', title: '無動作'}
-                ],
-                engine_title: '引擎:',
-                engine_option: [
-                    {value: '0', title: 'Google'}
-                ]
-            },
-            down: {
-                title: 'DOWN: ',
-                option: [
-                    {value: '0', title: '前景'},
-                    {value: '1', title: '背景'},
-                    {value: '2', title: '無動作'}
-                ],
-                engine_title: '引擎:',
-                engine_option: [
-                    {value: '0', title: 'Google'}
-                ]
-            },
-            right: {
-                title: 'RIGHT: ',
-                option: [
-                    {value: '0', title: '前景'},
-                    {value: '1', title: '背景'},
-                    {value: '2', title: '無動作'}
-                ],
-                engine_title: '引擎:',
-                engine_option: [
-                    {value: '0', title: 'Google'}
-                ]
-            },
-            left: {
-                title: 'LEFT: ',
-                option: [
-                    {value: '0', title: '前景'},
-                    {value: '1', title: '背景'},
-                    {value: '2', title: '無動作'}
-                ],
-                engine_title: '引擎:',
-                engine_option: [
-                    {value: '0', title: 'Google'}
-                ]
-            }
-        };
+    TH.import_tmpl(['simple_option'], function(r) { 
+        var text_option = [
+            {value: '0', title: '在前景新分頁中搜尋'},
+            {value: '1', title: '在背景新分頁中搜尋'},
+            {value: '2', title: '在目前分頁中搜尋'},
+            {value: '3', title: '在當前頁面找尋文字並Highlight'},
+            {value: '99', title: '無動作'}
+        ];
         
-        var ns = ['Gesture for draging Link', 'Gesture for draging Text', 'Gesture for draging Image'] // namespace
+        var link_option = [
+            {value: '0', title: '在前景新分頁中打開連結'},
+            {value: '1', title: '在背景新分頁中打開連結'},
+            {value: '2', title: '在目前分頁中打開連結'},
+            {value: '99', title: '無動作'}
+        ];
+        
+        var image_option = [
+            {value: '0', title: '在前景新分頁中打開連結'},
+            {value: '1', title: '在背景新分頁中打開連結'},
+            {value: '2', title: '在目前分頁中打開連結'},
+            {value: '99', title: '無動作'}
+        ];
+        
+        var engine_option = [
+            {value: '0', title: 'Google'}
+        ];
+        
+        // namespace
+        var ns = [
+            {},
+            {
+                legend: 'Gesture for draging Link',
+                option: link_option
+            },
+            {
+                legend: 'Gesture for draging Text',
+                option: text_option,
+                engine_title: 'Engine:',
+                engine_option: engine_option
+            },
+            {
+                legend: 'Gesture for draging Image',
+                option: image_option
+            }
+        ];
         for(var i=1; i<=3; i++) {
-            opts.legend = ns[i-1];
+            // normal opts
+            var opts = {
+                legend: ns[i].legend,
+                up: { title: 'UP: ', option: [] },
+                down: { title: 'DOWN: ', option: [] },
+                right: { title: 'RIGHT: ', option: [] },
+                left: { title: 'LEFT: ', option: [] }
+            };
+            
+            $.each(['up', 'down', 'right', 'left'], function(k, v) {
+                opts[v].option = ns[i].option;
+                
+                if(ns[i].engine_title) {
+                    opts[v]['engine_title'] = ns[i].engine_title;
+                }
+                
+                if(ns[i].engine_option) {
+                    opts[v]['engine_option'] = ns[i].engine_option;
+                }
+            });
+
             var content = tmpl('template-simple_option', opts);
             $('#tabs-'+i).html(content);
         }
