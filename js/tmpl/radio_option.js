bf7299446e2f1a20f4574f6728a37ef26066f31e
Ray Tseng <mutarock@gmail.com>
2013-12-02 10:08:35 +0800

    Add css parameter

--- js/tmpl/radio_option.js
+++ js/tmpl/radio_option.js
@@ -7,7 +7,7 @@
     {% for(var x in set) { %}
         {% var key = set[x]; %}
         {% if(o[key]) { %}
-        <li class="o_item">
+        <li class="rad_item">
             <label>{%=o[key].title%}</label>
             <input class="rad_action" id="{%=o.prefix%}_{%=key%}" name="selected_mode" type="radio">
             </input>
